97a227c1cf2c9158c5e227fb7f348d8b307f4763
Benjamin Kraft <benjamin_e_kraft@homedepot.com>
2020-02-28 16:53:55 -0600

    Kraft - Commit Feb 28 4:53pm CT

---
+++ exercise003.js
@@ -0,0 +1,7 @@
+function foo (){
+    quux = 7;
+    var bar;
+    function zip () {
+        var quux = 13;
+    }
+};
---
+++ exercise004.js
@@ -0,0 +1,9 @@
+function foo (){
+    quux = 7;
+    var bar;
+    return zip;
+    function zip () {
+        var quux = 13;
+        bar = true;
+    }
+};
